90d15ae7aae561656668f6a6f0ebc30affb30157
Harshit Vashisth <109802813+Harshit-Vashisth@users.noreply.github.com>
2023-03-21 17:29:13 +0530

    Update classifyheart.py

---
+++ heart/classifyheart.py
@@ -0,0 +1,11 @@
+###heart disease prediction
+
+#WORK FLOW
+
+#1 heart data- this data consist of several phealth parameter corresponding to person health
+#2 processing the data -- we cannot feed the raw data into out machine learniing model  therfore we will process this data
+#3 spliting the data - now we will split the data into training and test data ,,, training data to train or model and test data to test the data
+#4 In this case we are going to use logistic regression model becz thisparticular use case in binaary casscification here we are going to classify wheather a person has disease heart or not
+#5 in binnary classification model logistic regression model is very useful
+#6 now we will do evaluation on our trained model to check its performance sp after we will get a trained loggistic regressin model and to this model when we feed new data our model can predict wheather a person has heart disease or not
+
