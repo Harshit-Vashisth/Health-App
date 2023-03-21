35e7536ac80a4fbe8c4b7ebd33213b42e839afc2
Harshit Vashisth <109802813+Harshit-Vashisth@users.noreply.github.com>
2023-03-21 23:30:27 +0530

    Update classifyheart.py

--- heart/classifyheart.py
+++ heart/classifyheart.py
@@ -39,3 +39,80 @@ print(data.head())
 print(data.tail())
 
 
+#number of rows and coloumns in our dataset
+print(data.shape)
+
+
+#getting more info about the data
+print(data.info())
+
+##checking for missing values
+print(data.isnull().sum())
+
+#alayse the data
+#statistical measures about the data
+print(data.describe())
+
+
+#lets see hwo many people have heart disease and how many of them dont have that
+#checking the distribution of target varible
+print(data['target'].value_counts())
+#1 -.yes(defective)   0 is no
+
+
+#spliting the feature and the target
+x=data.drop(columns='target',axis=1)
+y=data['target']
+
+print(x)
+print(y)
+
+## nwo we have to feed x &  y to our ml model now before doing that but before we have to split our data into training data and test data
+#spliting data
+x_train,x_test,y_train,y_test=train_test_split(x,y,test_size=0.2,stratify=y,random_state=2)  #20% as test Stratify split the data similar propotion of 0 and 1
+
+
+#lets check  number of train and test data
+print(x.shape,x_train.shape,x_test.shape)
+
+
+###MODEL TRAINING
+##LOGISTIC REGRESION -> also for classification
+model =LogisticRegression()  # loading one instance of logistic reg model
+
+
+##training our machine learning model logistic reg model
+model.fit(x_train,y_train)   # it will find the pattern between these feature and the corresponding targets
+
+##model evaluation
+## ACCURACY SCORE -> model will be asked to predict the target and these prdeict valeu will be compared to the orignal target value
+
+##accuracy on training data
+xtrain_pred=model.predict(x_train)
+train_dataacc=accuracy_score(xtrain_pred,y_train)
+
+print("Accuracy on Training data :",train_dataacc)
+
+##accuracy score of testing data
+xtest_pred=model.predict(x_test)
+test_dataacc=accuracy_score(xtest_pred,y_test)
+
+print("Accuracy on Training data :",test_dataacc)
+
+##accuracy score in both shoudl be similar  ,,, overfittting comes there
+
+
+#BUILDING A PREDICTIVE SYSTEM
+input_data=(49,1,1,130,266,0,1,171,0,0.6,2,0,2)
+#processing on this data  change input data to numpy array
+data_nump=np.asarray(input_data)
+data_nump=data_nump.reshape(1,-1)  # otherwise it will assume it to bw 302  as we are predicting for one data point only
+
+pred=model.predict(data_nump)
+print(pred)
+
+if(pred[0]==0):
+    print("Person does not have heart problem ")
+else:
+    print("You are having heart problem")
+
