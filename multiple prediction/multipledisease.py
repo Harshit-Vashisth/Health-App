977c424b8e2be030bcd1a42084f4a537e921dd3e
Harshit Vashisth <109802813+Harshit-Vashisth@users.noreply.github.com>
2023-03-28 11:49:36 +0530

    Update multipledisease.py

--- multiple prediction/multipledisease.py
+++ multiple prediction/multipledisease.py
@@ -23,7 +23,7 @@ with st.sidebar:
                             ['Heart Disease Prediction',#list for what are diffrent pages we want as we have seen multiple web apps
                              'Parkinsons Disease Prediction'],
                             
-                            icons=['activity','P-circle'],
+                            icons=['activity','person'],
                             
                             default_index=0)#default index =0 means the page which is selected is 0 that is heart
 
@@ -32,6 +32,22 @@ with st.sidebar:
 if(selected=='Heart Disease Prediction'):
 #page title
     st.title('Heart Disease Prediction using ML')
+    
+    #inputing from user   st is for stemlit text_input is for those input fields and the string we want to display about the input level
+    age=st.text_input('Age')
+    sex=st.text_input('Sex')
+    cp=st.text_input('Chest Pain Types')
+    trestbps=st.text_input('Resting Blood pressure')
+    chol=st.text_input('Serum Cholestoral in mg/dl')
+    fbs = st.text_input('Fasting Blood Sugar > 120 mg/dl')
+    restecg = st.text_input('Resting Electrocardiographic results')
+    thalach = st.text_input('Maximum Heart Rate achieved')
+    exang = st.text_input('Exercise Induced Angina')
+    oldpeak = st.text_input('ST depression induced by exercise')
+    slope = st.text_input('Slope of the peak exercise ST segment')
+    ca = st.text_input('Major vessels colored by flourosopy')
+    thal = st.text_input('thal: 0 = normal; 1 = fixed defect; 2 = reversable defect')
+    
     
     
 #parkinson Disease Prediction page
