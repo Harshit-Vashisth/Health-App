a0166d598f1c7f660b3bf09e3a87aa478afef907
Harshit Vashisth <109802813+Harshit-Vashisth@users.noreply.github.com>
2023-04-17 00:06:29 +0530

    By harshit

--- Covid/changedata.py
+++ Covid/changedata.py
@@ -7,7 +7,7 @@ df = pd.read_csv('Cleaned-Data.csv')
 df['covid_range'] = df.apply(lambda row: ''.join(map(str, [row['Severity_None'], row['Severity_Mild'], row['Severity_Moderate'], row['Severity_Severe']])), axis=1)
 
 # Replace the combined values with 1, 2, 3, and 4
-df['covid_range'] = df['covid_range'].replace({' 1000': 1, '0100': 2, '0010': 3, '0001': 4})
+df['covid_range'] = df['covid_range'].replace({'1000': 1, '0100': 2, '0010': 3, '0001': 4})
 
 # Save the modified CSV file
 df.to_csv('Coviddata.csv', index=False)
--- Covid/changedata2.py
+++ Covid/changedata2.py
@@ -4,10 +4,10 @@ import pandas as pd
 df = pd.read_csv('Coviddata.csv')
 
 # Combine the four columns with 0 and 1 as values
-df['covid_range'] = df.apply(lambda row: ''.join(map(str, [row['Age_0-9'], row['Age_10-19'], row['Age_20-24'], row['Age_25-59'], row['Age_60+']])), axis=1)
+df['Age'] = df.apply(lambda row: ''.join(map(str, [row['Age_0-9'], row['Age_10-19'], row['Age_20-24'], row['Age_25-59'], row['Age_60+']])), axis=1)
 
 # Replace the combined values with 1, 2, 3, and 4
-df['covid_range'] = df['covid_range'].replace({'10000': 1, '01000': 2, '00100': 3, '00010': 4,'00001': 5})
+df['Age'] = df['covid_range'].replace({'10000': 1, '01000': 2, '00100': 3, '00010': 4,'00001': 5})
 
 # Save the modified CSV file
 df.to_csv('Covid-data.csv', index=False)
