da341402ac774124b4a3aad884d136842d2a0e44
Harshit Vashisth <109802813+Harshit-Vashisth@users.noreply.github.com>
2023-04-16 23:54:59 +0530

    by harshit

--- Covid/changedata.py
+++ Covid/changedata.py
@@ -1,13 +1,13 @@
 import pandas as pd
 
 # Read the CSV file
-df = pd.read_csv('your_file.csv')
+df = pd.read_csv('Cleaned-Data.csv')
 
 # Combine the four columns with 0 and 1 as values
-df['combined'] = df.apply(lambda row: ''.join(map(str, [row['col1'], row['col2'], row['col3'], row['col4']])), axis=1)
+df['covid_range'] = df.apply(lambda row: ''.join(map(str, [row['Severity_None'], row['Severity_Mild'], row['Severity_Moderate'], row['Severity_Severe']])), axis=1)
 
 # Replace the combined values with 1, 2, 3, and 4
-df['combined'] = df['combined'].replace({'0000': 1, '0001': 2, '0010': 3, '0011': 4})
+df['covid_range'] = df['covid_range'].replace({' 1000': 1, '0100': 2, '0010': 3, '0001': 4})
 
 # Save the modified CSV file
-df.to_csv('your_modified_file.csv', index=False)
+df.to_csv('Coviddata.csv', index=False)
