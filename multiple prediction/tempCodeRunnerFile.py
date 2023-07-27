f6a8589daa2df78930d8ac7269c8c8c5170c729d
Harshit Vashisth <109802813+Harshit-Vashisth@users.noreply.github.com>
2023-07-27 23:44:57 +0530

    by harshit

--- multiple prediction/tempCodeRunnerFile.py
+++ multiple prediction/tempCodeRunnerFile.py
@@ -1,3 +1,7 @@
-
-# with open('style.css") as f:
-#     st.markdown(f"<style>{f.read()}</style>",unsafe_allow_html=True)
+ # gen= st.radio("**Resting Electrocardiographic results**",( 'Normal','Borderline abnormality','Abnormality, include the presence of an arrhythmia'))
+        # if(gen=="Normal"):
+        #     restecg=0
+        # if(gen=="Borderline abnormality"):
+        #     restecg=1
+        # else :
+        #     restecg=2
